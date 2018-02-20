73d5fb4d8e2921ba344a145af85eba07570f740c
alexeyyafa <frai.92@gmail.com>
2018-02-20 23:24:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,29 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+ preferences.sort(function(a, b){return a - b});
+
+function arrayunique(preferences) {
+    var tmp_arr = new Array();
+    for (var i = 0; i < preferences.length; i++) {
+        if (tmp_arr.indexOf(preferences[i]) == "-1") {
+            tmp_arr.push(preferences[i]);
+        }
+    }
+    return tmp_arr;
+}
+
+function arraygroups() {
+    var groups = 0;
+    for(var i=0; i < arrayunique(preferences).length; i++){
+
+        if(arrayunique(preferences)[i] + 1 == arrayunique(preferences)[i+1] && arrayunique(preferences)[i] + 2 ==arrayunique(preferences)[i+2]){
+
+            ++groups;
+            i += 2;
+        }
+    }
+
+return groups;
+}
 };

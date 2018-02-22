9c181025bb75b5b6ef377238b818eb5a225f417a
alexeyyafa <frai.92@gmail.com>
2018-02-22 11:14:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,31 +3,25 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-function arrayunique(preferences) {
-	preferences.sort(function(a, b){return a - b});
-    var tmp_arr = new Array();
-    for (var i = 0; i < preferences.length; i++) {
-        if (tmp_arr.indexOf(preferences[i]) == "-1") {
-            tmp_arr.push(preferences[i]);
-        }
-    }
-    return tmp_arr;
+ var checked = []; // список с индексами, которые проверяли 
+  var count = 0
+  var arr = preferences;
 
-}
-
-
-function arraygroups(preferences = []) {
-    var groups = 0;
-    for(var i=0; i < arrayunique(preferences).length; i++){
-
-        if(arrayunique(preferences)[i] + 1 == arrayunique(preferences)[i+1] && arrayunique(preferences)[i] + 2 ==arrayunique(preferences)[i+2]){
-
-            ++groups;
-            i += 2;
-        }
-    }
-
-return groups;
-}
-return arraygroups(preferences);
+  for (var k = 0; k + 3 <= arr.length; k += 1) {
+      var first = getNext(k);
+      var a = arr[first];
+      var b = arr[--a];
+      var c = arr[--b];
+      if (first == --c && a != b && a != c && b != c) {
+          checked.push(first);
+          checked.push(a--);
+          checked.push(b--);
+          count++;
+      }
+  }
+  function getNext(n) {
+      while (checked.includes(n)) n++
+          return n;
+  }
+  return (count);
 };
